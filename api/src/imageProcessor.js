2c8827959bf31a9768286e5a881ff5531c15db92
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-04-12 17:40:42 +0100

    module 5 part 9

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -14,7 +14,11 @@ const imageProcessor = (filename) => {
     const monochromeDestination = uploadPathResolver('monochrome-' + filename)
     return new Promise((resolve, reject) => {
         if (isMainThread) {
-
+            try {
+                const resizeWorker = new Worker(pathToResizeWorker, ({ workerData: { source: sourcePath, destination: resizedDestination } }))
+            } catch (error) {
+                reject(error)
+            }
         }
         else {
             reject(new Error('not on main thread'))
