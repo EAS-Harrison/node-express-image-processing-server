cc111052d1ed053034bed1424cbf629bae218c8f
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-04-12 17:47:05 +0100

    .

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -15,8 +15,12 @@ const imageProcessor = (filename) => {
     return new Promise((resolve, reject) => {
         if (isMainThread) {
             try {
-                const monochromeWorker = new Worker(pathToMonochromeWorker, ({ workerData: { source: sourcePath, destination: monochromeDestination } }))
-                const resizeWorker = new Worker(pathToResizeWorker, ({ workerData: { source: sourcePath, destination: resizedDestination } }))
+                const monochromeWorker = new Worker(pathToMonochromeWorker, {
+                    workerData: {
+                        source: sourcePath,
+                        destination: monochromeDestination,
+                    },
+                }); const resizeWorker = new Worker(pathToResizeWorker, ({ workerData: { source: sourcePath, destination: resizedDestination } }))
             } catch (error) {
                 reject(error)
             }
